300f4b61ece332eb18107ea6f8fdee3212f92377
Immortal1235 <johenans7@gmail.com>
2025-05-04 14:25:08 +0200

    Delete install-node.ps1

--- install-node.ps1
+++
@@ -1,8 +0,0 @@
-# Example PowerShell script to install Node.js
-$nodejsUrl = "https://nodejs.org/dist/v16.17.0/node-v16.17.0-x64.msi"
-$installerPath = "$env:TEMP\nodejs-installer.msi"
-Invoke-WebRequest -Uri $nodejsUrl -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList '/quiet' -Wait
-node -v
-npm -v
-Remove-Item -Path $installerPath
